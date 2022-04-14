89faf825ca3a659afe46d0f08c586e5c85d60c07
Yash Kathiria <81464780+yashkathiria01@users.noreply.github.com>
2022-04-14 21:47:23 +0530

    Add files via upload

---
+++ Address-Book JAVA/Address_Book/src/mainpackage/Main.java
@@ -0,0 +1,44 @@
+package mainpackage;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		Scanner input = new Scanner(System.in);
+		int exit = 0;
+		int answer;
+		// we will loop until user wants to exit the application
+		do {
+			System.out.println("--------Welcome to Address Book---------");
+			System.out.println("Enter '1' to Add contact");
+			System.out.println("Enter '2' to Search contact");
+			System.out.println("Enter '3' to Print contact");
+			System.out.println("Enter '4' to Edit contact");
+			System.out.println("Enter '5' to Delete contact");
+			System.out.println("Enter '0' to Exit");
+			System.out.println(
+					"Do you want to print contacts, add contact, search for contact, edit contact or delete contact?");
+			System.out.println("Answer with '1', '2', '3', '4', '5' or '0' to exit application.");
+			try {// we handle the input of the user
+				answer = input.nextInt();
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+				answer = -1;
+			}
+			if (answer == 1)// according to user's input we go to each class
+				Add.add_contact();
+			else if (answer == 2)
+				Search.choose_field();
+			else if (answer == 3)
+				Print.show_contacts();
+			else if (answer == 4)
+				Change.choose_field();
+			else if (answer == 5)
+				Delete.choose_field();
+		} while (answer != exit);
+		System.out.println("Application terminating...");
+		input.close();
+	}
+}
---
+++ Address-Book JAVA/Address_Book/src/mainpackage/Print.java
@@ -0,0 +1,35 @@
+package mainpackage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Print extends Main{
+	public static void show_contacts() throws IOException, FileNotFoundException{
+		File file = new File(System.getProperty("user.dir")+"/src/contacts.txt");//we get the contact file 
+		System.out.println(file.getAbsolutePath());
+		System.out.println(file.getParentFile());
+		System.out.println(file.exists());
+		file.createNewFile();
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String currentLine;
+		boolean first = false;
+		String[] fields = new String[0];
+		while((currentLine = reader.readLine()) !=null) {//for each line in txt file
+			if(!first) {//if it is the first line the line is the fields and we save them into an array
+				fields = currentLine.split(",");
+				first = true;
+			}
+			else {//for the rest lines we print the information
+				System.out.println("-------------------");
+				String[] info=currentLine.split(",");
+				for (int i = 0; i < fields.length; i++ ) {
+					System.out.println(fields[i] +": "+ info[i]);					
+				}
+			}
+		}
+		System.out.println("-------------------");
+		reader.close();
+	}
+}
